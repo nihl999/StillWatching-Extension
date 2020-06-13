17fae2a325b74e8f012e21b6ce81e992d5615bfe
Arthur Lorhan <arthurlohransilva@gmail.com>
2020-06-12 23:36:47 -0300

    Done? running tests

--- content.js
+++ content.js
@@ -1,3 +1,15 @@
+const observerConfig = {childList: true};
 const popupContainer = document.querySelector('ytd-popup-container');
-const confirmContainer = popupContainer.querySelector('yt-button-renderer#confirm-button');
-const confirmButton = confirmContainer.querySelector('paper-button#button')
+const callback = function(mutationsList, observer){
+    mutationsList.forEach(element => {
+        if(element.type === 'childList'){
+            const confirmContainer = popupContainer.querySelector('yt-button-renderer#confirm-button');
+            const confirmButton = confirmContainer.querySelector('paper-button#button');
+            confirmButton.click();
+            console.log('StillWatching acting'); 
+        }
+    });
+};
+const extObserver = new MutationObserver(callback);
+extObserver.observe(popupContainer, observerConfig);
+console.log('StillWatching running...');
