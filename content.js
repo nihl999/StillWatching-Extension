1f626a16ecee228e7685595d6ef4b7e49464c111
Arthur Lorhan <arthurlohransilva@gmail.com>
2020-06-12 21:33:27 -0300

    Discovering confirm button

---
+++ content.js
@@ -0,0 +1,3 @@
+const popupContainer = document.querySelector('ytd-popup-container');
+const confirmContainer = popupContainer.querySelector('yt-button-renderer#confirm-button');
+const confirmButton = confirmContainer.querySelector('paper-button#button')
